c2f85a6b63c522a195fca13a2b3d5c443a0ac363
Olayinka-2 <olayinkaisrael802@gmail.com>
2025-03-21 13:10:30 +0100

    Add basic form input

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,35 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className="container">
-        
+      <div className="main-container">
+          <header>
+            <img src="./src/asset/images/logo-mark.svg" alt="Brand logo" />
+            <p> Code Conf</p>
+          </header>
+          <main>
+            <div className="form-container">
+              <form action="#">
+                <h1>
+                  Your journey to code conf 2025 starts here!
+                </h1>
+                <p>Secure your spot at next&apos;s year biggest coding conference.</p>
+                <div className="form-input-container">
+                  <div className="input">
+                    <label htmlFor="fullName">Full Name </label>
+                    <input type="text" name='fullName' id='fullName'/>
+                  </div>
+                  <div className="input">
+                    <label htmlFor="email">Email Address </label>
+                    <input type="text" name='email' id='email'/>
+                  </div>
+                  <div className="input">
+                    <label htmlFor="github'">GitHub Username </label>
+                    <input type="text" name='github' id='github'/>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </main>
       </div>
     </>
   )
