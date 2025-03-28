b2340febe6112e5c2372d47a0addc6c0cf6f01f9
Olayinka-2 <olayinkaisrael802@gmail.com>
2025-03-28 12:22:51 +0100

    Add styles for responsiveness

--- src/component/ticket-generated.jsx
+++ src/component/ticket-generated.jsx
@@ -20,24 +20,25 @@ export default function GenerateTicket() {
                   </div>
 
                   <div className={styles["main-profile"]}>
-                  <div className={styles["address-container"]}>
-                        <div className={styles["address"]}>
-                           <img src={logo} alt="logo" className="img" />
-                           <p>Coding Conf</p>
+                     <div className={styles["address-container"]}>
+                           <div className={styles["address"]}>
+                              <img src={logo} alt="logo" className={styles["img"]} />
+                           </div>
+                           <div>
+                           <p className={styles["firstPara"]}>Coding Conf</p>
+                           <p className={styles["addressText"]}>    Mar 31, 2025 / Minna, Nigeria</p>
+                           </div>
                         </div>
-                        <div className={styles["addressText"]}>
-                           Mar 31, 2025 / Minna, Nigeria
-                        </div>
-                     </div>
-                     {/* <div className={styles["address-container"]}>
+
+                     <div className={styles["address-container-last"]}>
                         <div className={styles["address"]}>
                            <img src={avatarImage} alt="profile image" className={styles["profile-image"]} />
                         </div>
-                        <div className={styles["addressText"]}>
-                        <p>Olayinka Israel</p>
-                        <p> https://github.com/Olayinka-2/ </p>
+                        <div>
+                        <p className={styles["firstPara"]}>Olayinka Israel</p>
+                        <p  className={styles["addressText"]}> https://github.com/Olayinka-2/ </p>
                         </div>
-                     </div> */}
+                     </div>
                   </div>
                      
                {/* <div className={styles["success-card"]}>
